0abebbc86d8aa2b72a6f4b17a6646a2587e10807
Oscar Kemuel <34771800+oscarkemuel@users.noreply.github.com>
2025-05-14 18:11:59 -0300

    feat(accounts): implements create account. (#8)
    * feat(accounts): implements create account.
    
    * feat(accounts): allows negative balance.
    
    * fix(accounts): fix endpoint route.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { exampleRouter } from './exemple.routes';
+import { accountRouter } from './account.routes';
 
 const router = Router();
 
 router.use('/example', exampleRouter);
+router.use('/account', accountRouter);
 
 export { router };
---
+++ src/schemas/accountSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const accountSchema = z.object({
+  number: z.number().int().positive(),
+  balance: z.number(),
+});
+
+export const createAccountSchema = z.object({
+  number: z.number().int().positive(),
+});
+
+export type Account = z.infer<typeof accountSchema>;
