3449eb14e6466ac3abce08a48b60143b07a150e5
Daniel Lucena <drlucena@gmail.com>
2025-05-16 23:10:59 -0300

    feat(debit): implements debit from account. (#9)

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -12,8 +12,27 @@ export class AccountService {
 
     const all = await this.repo.getAll();
     all.push(account);
-    
+
     await this.repo.save(all);
     return account;
+  }
+
+  async debitFromAccount(number: number, amount: number): Promise<Account> {
+    const account = await this.repo.findByNumber(number);
+    if (account === null) throw new Error("There is no account with number " + number)
+
+    const all = await this.repo.getAll();
+    const updated = all.map(acc => {
+      if (acc.number === number) {
+        return { ...acc, balance: acc.balance - amount };
+      }
+      return acc;
+    });
+
+    await this.repo.save(updated);
+
+    const updatedAccount = updated.find(acc => acc.number === number);
+    if (!updatedAccount) throw new Error("Something went wrong updating the account");
+    return updatedAccount;
   }
 }
